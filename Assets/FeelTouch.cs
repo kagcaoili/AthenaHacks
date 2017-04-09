3574a2bf324bb522e71616fcc71b3138b67ec393
Nisha Yerunkar <nishasy@gmail.com>
2017-04-09 08:07:28 -0700

    pls just acknowledge the food or I will flip a table

---
+++ Assets/FeelTouch.cs
@@ -0,0 +1,19 @@
+﻿using VRTK;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeelTouch : VRTK_InteractableObject {
+
+	// Use this for initialization
+	void Start () {
+		if (IsTouched ()) {
+			Debug.Log ("You win");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
