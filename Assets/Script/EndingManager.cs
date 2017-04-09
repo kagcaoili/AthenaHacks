28f0b701a88e105c325bf81febec7c7252a7e38d
Kristin Agcaoili <kagcaoil@ucsd.edu>
2017-04-08 23:09:38 -0700

    Added tree and animation

---
+++ Assets/Script/EndingManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingManager : MonoBehaviour {
+
+    public GameObject bigTree;
+    public GameObject deadTree;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(SpawnFireworks());
+	}
+
+    IEnumerator SpawnFireworks()
+    {
+        yield return new WaitForSeconds(2.0f);
+
+        Debug.Log("StartTree");
+        StartTreeFalling();
+        StartFireworks();
+
+        yield return new WaitForSeconds(10.0f);
+
+        //fade to black or end game sequence
+
+    }
+
+    public void StartFireworks()
+    {
+        //start fireworks animations
+    }
+
+    public void StartTreeFalling()
+    {
+        //start tree falling animation
+        bigTree.GetComponent<Animation>().Play();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+
+    private EndingManager endgame;
+
+
+    [SerializeField]
+    private GameObject bigTreeSet;
+
+    [SerializeField]
+    private GameObject deadTreeSet;
+
+    private int taskCount = 0;
+
+    [SerializeField]
+    private int MAX_TASKS = 1;
+
+	// Use this for initialization
+	void Start () {
+        endgame = new EndingManager();
+        endgame.bigTree = bigTreeSet;
+        Debug.Log("set endgame");
+        //endgame.deadTree = deadTreeSet;
+
+        //endgame.StartTreeFalling();
+	}
+
+    public void IncrementTaskCount()
+    {
+        if (taskCount < MAX_TASKS)
+        {
+            taskCount++;
+        }
+
+        if (taskCount == MAX_TASKS)
+        {
+            endgame.StartTreeFalling();
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
