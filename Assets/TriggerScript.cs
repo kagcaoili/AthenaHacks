86eafa67c816e22600ce7367925f3a752416b433
Jenny Wong <jew007@ucsd.edu>
2017-04-09 04:57:37 -0700

    henlo

---
+++ Assets/TriggerScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerScript : MonoBehaviour {
+
+	public Color watercolor;
+
+	// Use this for initialization
+	void Start () { /*
+		MeshRenderer r = GetComponent<MeshRenderer> ();
+		Material why = new Material (r.material);
+		why.SetColor (watercolor);
+		r.material = why;*/
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/WaterScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterScript : MonoBehaviour {
+	
+	public Color blue;
+	MeshRenderer ren;
+	public Texture txtr;
+
+	// Use this for initialization
+	void Start () {
+		ren = GetComponent<MeshRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerExit(Collider other) {
+		Material currmat = ren.material;
+		Material newmat = new Material (currmat);
+		newmat.SetColor ("_Color", blue);
+		ren.material = newmat;
+
+		ren.material.mainTexture = txtr;
+
+		Debug.Log ("that bottle dippity dip");
+	}
+}
