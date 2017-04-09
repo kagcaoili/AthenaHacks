3d4e84e232d2df75e6c5fab5361dfcb72702ffd7
Nisha Yerunkar <nishasy@gmail.com>
2017-04-09 01:17:03 -0700

    can we grab the sticks

---
+++ Assets/Campfire.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Campfire : MonoBehaviour {
+
+	bool stickEntered = false;
+	int stickNumber = 0;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.GetComponent<Stick>() != null) {
+			stickEntered = true;
+			stickNumber++;
+			Debug.Log("stick entered " + stickNumber);
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.gameObject.GetComponent<Stick>() != null) {
+			stickEntered = false;
+			stickNumber--;
+		}
+	}
+
+	bool getStickEntered() {
+		return stickEntered;
+	}
+
+	int getStickNumber() {
+		return stickNumber;
+	}
+}
