00f164dac2e0c5351c2f32ccc98f5020e09ec829
Nisha Yerunkar <nishasy@gmail.com>
2017-04-09 07:38:42 -0700

    put code in update instead of start

--- Assets/berry.cs
+++ Assets/berry.cs
@@ -6,21 +6,11 @@ using UnityEngine;
 
 public class berry : VRTK_InteractableObject {
 
-	static int berriesEaten = 0;
+	private static int berriesEaten = 0;
 
 	// Use this for initialization
 	void Start () {
-		if (IsUsing ()) {
-			berriesEaten++;
-			//add eating noise here maybe
-			Debug.Log("berries eaten: " + berriesEaten);
-			Destroy (this);
-		}
 
-		if (berriesEaten == 10) { // end task
-			// +1 task script here
-			Debug.Log("you've eaten ten berries! you're done");
-		}
 	}
 
 	public override void StartUsing(GameObject usingObject) {
@@ -33,9 +23,18 @@ public class berry : VRTK_InteractableObject {
 		base.StopUsing (usingObject);
 	}
 
-	protected override void Update() {
-		base.Update();
+	void Update() {
+		if (IsGrabbed ()) {
+			berriesEaten++;
+			//add eating noise here maybe
+			Debug.Log("berries eaten: " + berriesEaten);
+			Destroy (this);
+		}
+
+		if (berriesEaten == 10) { // end task
+			// +1 task script here
+			Debug.Log("you've eaten ten berries! you're done");
+		}
 	}
-
-
+		
 }
