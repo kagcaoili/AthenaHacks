04edff1e9c910cd292c764c396b568c4095ac05d
Nisha Yerunkar <nishasy@gmail.com>
2017-04-09 03:32:23 -0700

    stick counting test 1

--- Assets/Stick.cs
+++ Assets/Stick.cs
@@ -1,16 +1,48 @@
-﻿using System.Collections;
+﻿using VRTK;
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Stick : MonoBehaviour {
+public class Stick : VRTK_InteractableObject {
+
+	bool isUsed = false;
+	int counter = 0;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public override void StartUsing(GameObject usingObject) {
+
+		base.StartUsing (usingObject);
+		isUsed = true;
+	}
+
+	public override void StopUsing(GameObject usingObject) {
+
+		base.StopUsing (usingObject);
+		isUsed = false;
+		counter = 0;
+	}
+
+	protected override void Update() {
+		base.Update();
+		if (counter >= 6) {
+			Debug.Log ("PLS");
+		}
+	}
+
+	public bool stickIsHeld() {
+		return isUsed;
+	}
+
+	void OnCollisionEnter(Collision other) {
+		if (IsGrabbed ()) {
+			if (other.gameObject.GetComponent<Stick> () != null) { // is a stick
+				counter++;
+			}
+		}
 	}
 }
